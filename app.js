08089358297d52456c2be74162d7e68a47b61259
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-10-06 08:35:21 +0545

    map method explained

--- app.js
+++ app.js
@@ -66,3 +66,24 @@ account1.movements.forEach((mov, i) => {
 
   containerMovements.insertAdjacentHTML('afterbegin', html);
 });
+
+const movements = [200, 450, -400, 3000, -650, -130, 1300];
+
+// The map Method
+const eurToUsd = 1.1;
+const movementsUSD = movements.map(function (mov) {
+  return mov * eurToUsd;
+});
+// const movementsUSD = movements.map(mov => mov * eurToUsd);
+// console.log(movements);
+// console.log(movementsUSD);
+// const movementsUSDfor = [];
+// for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+// console.log(movementsUSDfor);
+// const movementsDescriptions = movements.map(
+//   (mov, i) =>
+//     `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+//       mov
+//     )}`
+// );
+console.log(movementsUSD);
